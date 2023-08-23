360b44290f93398f7b02d2417da82d60f66681f7
neewamp <ns196414@ohio.edu>
2023-08-22 21:36:15 -0400

    Working through understanding more about typescript.  Unions in the presence of structural types and no real way to distinguish different parts of the union without adding a tag is mildly concerning

--- src/compiler/control.ts
+++ src/compiler/control.ts
@@ -1,6 +1,8 @@
 // Niave duplication of solver-aided-stpa's control.py data structures 
 // Mainly doing this to get a little more familiar with typescript 
 
+import { Err } from "pratica";
+
 // Debating class vs type
 // Class allows for the definition of member function but seems to do weird stuff with member function definitions
 // [Object object] when trying to use a toString function???
@@ -12,16 +14,39 @@ type Ident = {
 
 function IdentToString(i: Ident): string {
     if(i.qualifier) {
-        return i.qualifier.toString() + '.' + i.name;
+        return IdentToString(i.qualifier) + '.' + i.name;
     }
     else {
         return i.name;
     }
 }
+function IdentOfList(l: string[]): Ident {
+    if(l?.length) {
+        if(l?.length == 1) {
+            return {name: l[0]};
+        }
+        else {
+            return {
+                qualifier: IdentOfList(l.slice(0, -1)),
+                name: l[-1]
+            }
+        }
+    }
+    else {
+        throw new Error("IdentOfList: empty list")
+    }
+
+}
+
+function IdentOfString(s : string): Ident {
+    return IdentOfList(s.split('.'));
+}
 
 type VarDecl = {
     name: string;
 }
+
+type Test = VarDecl | Ident 
 
 function VarDeclToString(v : VarDecl): string {
     return v.name;
@@ -29,7 +54,43 @@ function VarDeclToString(v : VarDecl): string {
 
 let v : VarDecl = {name: "hi"};
 
+// Experimenting with how to get this to Alex's python app
+// console.log(JSON.stringify(v))
+
 type Type = "int" | "bool" | Ident
+
+let t_ex: Type = "int";
+
+interface TypePattern<T> {
+    Bool: (s: "bool") => T;
+    Int: (b: "int") => T;
+    Ident: (n: Ident) => T;
+  }
+
+// Not a problem for me wanting to print a string 
+// Representation of this language 
+// But I really don't think I like the combination
+// of structural typing with unions...
+function TypeToString(t : Type): string {
+    if((t as Ident).name !== undefined){
+        return IdentToString(t as Ident);
+    }
+    else if(t == "bool") {
+        return "bool";
+    }
+    else {
+        return "int";
+    }
+    
+}
+/* function isFish(pet: Fish | Bird): pet is Fish {
+    return (pet as Fish).swim !== undefined;
+  }
+ */
+let t_lit_ex: Type = 
+ {qualifier: {name: "hmm"}, name: "hi"};
+
+console.log(TypeToString(t_lit_ex));
 
 type FinTypeDecl = {
     name: string;
@@ -71,7 +132,8 @@ type Expr = Ident | LiteralExp | UnaryExpr | BinaryExpr
 
 type Action = {
     name: string
-    contraints: Expr[]
+    allowed: Expr[]
+    required: Expr[]
 }
 
 type System = {
